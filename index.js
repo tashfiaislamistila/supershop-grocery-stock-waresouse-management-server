bf3b26d881fc4916bfb1f2c10f5e190539be35d6
tashfiaislamistila <tashfia.cse@gmail.com>
2022-05-07 18:29:04 -0700

    create api for individual id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, MongoCursorExhaustedError } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -27,6 +27,13 @@ async function run() {
             const groceries = await cursor.toArray();
             res.send(groceries);
         });
+
+        app.get('/grocery/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const grocery = await groceryCollection.findOne(query);
+            res.send(grocery);
+        })
 
     }
 
