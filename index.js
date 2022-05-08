9363e914b9dda384f3bce1f9482192bc1b7399fd
tashfiaislamistila <tashfia.cse@gmail.com>
2022-05-07 18:07:04 -0700

    create api by get app

--- index.js
+++ index.js
@@ -17,14 +17,17 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
-
     try {
-
         await client.connect();
-        const groceryCollection = client.db('warehouseUser').collection('grocery');
-        const query = {};
-        const cursor = groceryCollection.find(query);
-        const groceries = await cursor.toArray();
+        const groceryCollection = client.db('superShopGrocery').collection('grocery');
+
+        app.get('/grocery', async (req, res) => {
+            const query = {};
+            const cursor = groceryCollection.find(query);
+            const groceries = await cursor.toArray();
+            res.send(groceries);
+        });
+
     }
 
     finally {
