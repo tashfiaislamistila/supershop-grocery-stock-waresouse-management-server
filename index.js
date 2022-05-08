1ea3984f393c35bd8afc023a5a40060d7baa476f
tashfiaislamistila <tashfia.cse@gmail.com>
2022-05-07 22:43:03 -0700

    delete api create

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { ObjectID } = require('bson');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -40,7 +41,15 @@ async function run() {
             const newInventory = req.body;
             const result = await groceryCollection.insertOne(newInventory);
             res.send(result);
-        })
+        });
+
+        //DELETE 
+        app.delete('/grocery/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectID(id) };
+            const result = await groceryCollection.deleteOne(query);
+            res.send(result);
+        });
 
     }
 
