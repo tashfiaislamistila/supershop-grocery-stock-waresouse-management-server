a77dbc8580cf960dca8c0e9e29f220c07d2a067a
tashfiaislamistila <tashfia.cse@gmail.com>
2022-05-07 17:57:31 -0700

    cursor add and to array add

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, MongoCursorExhaustedError } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -15,15 +15,24 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7xsh1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('warehouse management DB connected');
-    // perform actions on the collection object
-    client.close();
-});
 
+async function run() {
 
+    try {
 
+        await client.connect();
+        const groceryCollection = client.db('warehouseUser').collection('grocery');
+        const query = {};
+        const cursor = groceryCollection.find(query);
+        const groceries = await cursor.toArray();
+    }
+
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Running warehouse server');
